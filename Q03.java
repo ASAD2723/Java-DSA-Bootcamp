642b1f5df8276945661b3f76ed516de327f5820a
ASAD2723 <khanasad323@gmail.com>
2021-09-22 22:05:20 +0530

    Area of Rectangle

---
+++ Q03.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Q02{
+    public static void main(String[],args){
+        System.out.print("Enter LENGTH of rectangle : ");
+        Scanner in = new Scanner(Sysytem.in);
+        int length = in.nextInt();
+        System.out.print("Enter BREADTH of triangle : ");
+        Scanner in = new Scanner(Sysytem.in);
+        int breadth = in.nextInt();
+        // Area of Rectangle
+        float area_of_rectangle = 0.5 * (base * height);
+        System.out.print("Area of RECTANGLE is " + area_of_rectangle);
+    }
+}
